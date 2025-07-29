4ca391670671ef6a18c7fba95d40ddd6c45b8791
Tuglea Cristian <tugleacristian@gmail.com>
2025-07-29 17:33:58 +0300

    Add restart game method

--- main.py
+++ main.py
@@ -26,10 +26,8 @@ class Game():
             if self.board[x-1][y-1] == 0:
                 return 1
             else:
-                print("Wrong move!")
                 return 0
         else:
-            print("Please type correct coordinates!")
             return 0
 
     def display_board(self):
@@ -46,44 +44,70 @@ class Game():
             a = 0
             if self.board[i][a] == self.board[i][a+1] == self.board[i][a+2]:
                 return self.board[i][a]
+                self.win = 1
             elif self.board[a][i] == self.board[a+1][i] == self.board[a+2][i]:
                 return self.board[a][i]
+                self.win = 1
         if self.board[0][0] == self.board[1][1] == self.board[2][2]:
             return self.board[0][0]
+            self.win = 1
         elif self.board[0][2] == self.board[1][1] == self.board[2][0]:
             return self.board[1][1]
+            self.win = 1
         else:
             return 0
+
+    def reload(self):
+        self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.turn = 2
+        self.win = 0
 
 
 game1 = Game()
 subprocess.run("clear")
-
-while game1.win == 0:
-
-    print("""
+'''
+print("""
 ▀▀█▀▀ ▀█▀ ░█▀▀█ 　 ▀▀█▀▀ ─█▀▀█ ░█▀▀█ 　 ▀▀█▀▀ ░█▀▀▀█ ░█▀▀▀
 ─░█── ░█─ ░█─── 　 ─░█── ░█▄▄█ ░█─── 　 ─░█── ░█──░█ ░█▀▀▀
 ─░█── ▄█▄ ░█▄▄█ 　 ─░█── ░█─░█ ░█▄▄█ 　 ─░█── ░█▄▄▄█ ░█▄▄▄""")
-    print("\n Welcome to TicTacToe!")
-    print("To specify the move you want to make, specify a value for row,column (like 1,3)")
+print("\n Welcome to TicTacToe!")
+'''
+
+while game1.win == 0:
+
+    print("To specify the move you want to make, type a value for row,column (like 1,3)")
 
     game1.display_board()
 
     if game1.check_win() == 1:
         print("Player O wins!")
-        break
+        choice = input("""Do you want to play again? (pres y for 'yes')
+>> """)
+        if choice == 'y':
+            game1.reload()
+            subprocess.run("clear")
+            continue
+        else:
+            break
+
     elif game1.check_win() == 2:
         print("Player X wins!")
-        break
+        choice = input("""Do you want to play again? (press y for 'yes')
+>> """)
+        if choice == 'y':
+            game1.reload()
+            subprocess.run("clear")
+            continue
+        else:
+            break
 
-    move = input(f"Player {"X" if game1.turn == 2 else "O"} move>> ")
+    move = input(f"Player {"X" if game1.turn == 2 else "O"}>> ")
     move_list = move.split(',')
     if game1.check_move(move_list[0], move_list[1]):
         game1.make_move(move_list[0], move_list[1])
     else:
+        subprocess.run("clear")
+        print("Wrong coordinates!")
         continue
-
-    subprocess.run("clear")
 
     subprocess.run("clear")
